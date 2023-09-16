b7fbe25ad9fcf8863d607a2e2f6e4041bd369b80
omarelnahs <omarelnahas1141@email.com>
2023-09-16 16:45:48 +0300

    a

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const TodoList = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className='header text-center'>
+                <h3>Todo List</h3>
+                <button 
+                    className='btn btn-primary mt-2'
+                >
+                    Create Task
+                </button>
+            </div>
+            <div className='task-container'>
+            </div>
+        </>
     );
 };
 
